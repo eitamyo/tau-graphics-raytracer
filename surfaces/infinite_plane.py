8df83969c4d922b913e55afa54371d9a48d391a2
Eitam Yogev <eitamyo@gmail.com>
2025-12-15 20:42:47 +0200

    njit

--- surfaces/infinite_plane.py
+++ surfaces/infinite_plane.py
@@ -1,29 +1,35 @@
 import numpy as np
+from numba import njit
+
+@njit
+def intersect_plane(normal, offset, ray_origin: np.ndarray, ray_direction: np.ndarray):
+    # Plane equation: normal · (P) = offset
+    # Ray equation: P = ray_origin + t * ray_direction
+    # Substitute ray equation into plane equation:
+    # normal · (ray_origin + t * ray_direction) = offset
+    # => normal · ray_origin + t * (normal · ray_direction) = offset
+    # => t = (offset - normal · ray_origin) / (normal · ray_direction)
+
+    denom = np.dot(normal, ray_direction)
+    if abs(denom) < 1e-6:
+        return None  # Ray is parallel to the plane
+
+    numer = offset - np.dot(normal, ray_origin)
+    t = numer / denom
+    if t >= 0:
+        return t
+    else:
+        return None # Intersection is behind the ray origin
+      
 
 class InfinitePlane:
     def __init__(self, normal, offset, material_index):
         self.normal = np.array(normal)
         self.offset = offset
         self.material_index = material_index
-    
+
     def intersect(self, ray_origin: np.ndarray, ray_direction: np.ndarray):
-        # Plane equation: normal · (P) = offset
-        # Ray equation: P = ray_origin + t * ray_direction
-        # Substitute ray equation into plane equation:
-        # normal · (ray_origin + t * ray_direction) = offset
-        # => normal · ray_origin + t * (normal · ray_direction) = offset
-        # => t = (offset - normal · ray_origin) / (normal · ray_direction)
-
-        denom = np.dot(self.normal, ray_direction)
-        if abs(denom) < 1e-6:
-            return None  # Ray is parallel to the plane
-
-        numer = self.offset - np.dot(self.normal, ray_origin)
-        t = numer / denom
-        if t >= 0:
-            return t
-        else:
-            return None # Intersection is behind the ray origin
+        return intersect_plane(self.normal, self.offset, ray_origin, ray_direction)
         
     def get_normal(self, point):
         return self.normal
--- surfaces/sphere.py
+++ surfaces/sphere.py
@@ -1,35 +1,37 @@
 from numba import njit
 import numpy as np
 
+@njit
+def intersect_sphere(ray_origin: np.ndarray, ray_direction: np.ndarray, sphere_pos: np.ndarray, sphere_radius: float):
+    # ||(ray_origin + t*ray_direction - pos)||^2 = radius^2
+    # <=> (ray_origin + t*ray_direction - pos)·(ray_origin + t*ray_direction - pos) = radius^2
+    # <=> (ray_direction·ray_direction)t^2 + 2(ray_direction·(ray_origin - pos))t + (ray_origin - pos)·(ray_origin - pos) - radius^2 = 0
+    # At^2 + Bt + C = 0
+    oc = ray_origin - sphere_pos
+    A = 1  # ray_direction is normalized
+    B = 2 * np.dot(oc, ray_direction)
+    C = np.dot(oc, oc) - sphere_radius ** 2
+    discriminant = B ** 2 - 4 * A * C
+    if discriminant < 0:
+        return None
+    sqrt_disc = discriminant ** 0.5
+    t1 = (-B - sqrt_disc) / (2 * A)
+    t2 = (-B + sqrt_disc) / (2 * A)
+    if t1 >= 0:
+        return t1
+    elif t2 >= 0:
+        return t2
+    else:
+        return None
+
 class Sphere:
     def __init__(self, position, radius, material_index):
         self.position = np.array(position)
         self.radius = radius
         self.material_index = material_index
         
-    # @njit
     def intersect(self, ray_origin: np.ndarray, ray_direction: np.ndarray):
-        # ||(ray_origin + t*ray_direction - pos)||^2 = radius^2
-        # <=> (ray_origin + t*ray_direction - pos)·(ray_origin + t*ray_direction - pos) = radius^2
-        # <=> (ray_direction·ray_direction)t^2 + 2(ray_direction·(ray_origin - pos))t + (ray_origin - pos)·(ray_origin - pos) - radius^2 = 0
-        # At^2 + Bt + C = 0
-        oc = ray_origin - self.position
-        A = 1  # ray_direction is normalized
-        B = 2 * np.dot(oc, ray_direction)
-        C = np.dot(oc, oc) - self.radius ** 2
-        discriminant = B ** 2 - 4 * A * C
-        if discriminant < 0:
-            return None
-        else:
-            sqrt_disc = discriminant ** 0.5
-            t1 = (-B - sqrt_disc) / (2 * A)
-            t2 = (-B + sqrt_disc) / (2 * A)
-            if t1 >= 0:
-                return t1
-            elif t2 >= 0:
-                return t2
-            else:
-                return None
+        return intersect_sphere(ray_origin, ray_direction, self.position, self.radius)
     
     def get_normal(self, point):
         normal = (point - self.position) / self.radius
